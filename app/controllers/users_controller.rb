df2ca55e17250a56281ef5086eefb88427c46ac7
Josh <joshua@mymainpc.com>
2022-09-02 20:31:07 -0400

    Fixing CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,12 @@ class UsersController < ApplicationController
   def show
 #    @user = User.find(params[:id])
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password, :description)
+    end
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
