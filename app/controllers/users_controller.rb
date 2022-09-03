08967926ecb7f35e260a85527efa5e4ecbb523e3
Josh <joshua@mymainpc.com>
2022-09-03 03:42:54 -0400

    CRUD Finished, Paths broken

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit]
+  before_action :set_user, only: [:show, :edit, :update]
   def index
     @users = User.all
   end
@@ -21,7 +21,17 @@ class UsersController < ApplicationController
 
   def edit
   end
-
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @user.destroy
+    redirect_to root_path, status: :see_other
+  end
   private
     def user_params
       params.require(:user).permit(:username, :password, :description)
